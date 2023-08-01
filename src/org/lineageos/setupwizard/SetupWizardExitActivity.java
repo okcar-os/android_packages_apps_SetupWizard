1d2b628484d70a3f08081821a2f7d19ce2f844e5
Leo <leohuangyi@foxmail.com>
2023-08-01 21:53:29 +0800

    Make OkLauncher your default launcher Hide some settings interface
    Change-Id: Id9d29619a15b1fe05e7a7a420d7eaa59dbee398e

--- src/org/lineageos/setupwizard/SetupWizardExitActivity.java
+++ src/org/lineageos/setupwizard/SetupWizardExitActivity.java
@@ -23,11 +23,16 @@ import static org.lineageos.setupwizard.SetupWizardApp.LOGV;
 
 import android.annotation.Nullable;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ComponentName;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.util.Log;
 
 import org.lineageos.setupwizard.util.PhoneMonitor;
 import org.lineageos.setupwizard.util.SetupWizardUtils;
+import java.util.List;
 
 public class SetupWizardExitActivity extends BaseSetupWizardActivity {
 
@@ -36,6 +41,8 @@ public class SetupWizardExitActivity extends BaseSetupWizardActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initLauncher();
+
         if (LOGV) {
             Log.v(TAG, "onCreate savedInstanceState=" + savedInstanceState);
         }
@@ -59,4 +66,42 @@ public class SetupWizardExitActivity extends BaseSetupWizardActivity {
                 .addFlags(FLAG_ACTIVITY_NEW_TASK | FLAG_ACTIVITY_CLEAR_TASK));
     }
 
+    public void initLauncher (){
+		String ORIGINAL_LAUNCHER_PACKAGENAME = "com.okcaros.launcher";
+    	String ORIGINAL_LAUNCHER_CLASSNAME = "app.lawnchair.LawnchairLauncher";
+        Intent queryIntent = new Intent();
+		final PackageManager mPm = this.getPackageManager();
+        queryIntent.addCategory(Intent.CATEGORY_HOME);
+        queryIntent.setAction(Intent.ACTION_MAIN);
+
+        List<ResolveInfo> homeActivities = mPm.queryIntentActivities(queryIntent, 0);
+        if(homeActivities == null) {
+            return;
+        }
+
+        ComponentName defaultLauncher = new ComponentName(ORIGINAL_LAUNCHER_PACKAGENAME,
+                ORIGINAL_LAUNCHER_CLASSNAME);
+        int activityNum = homeActivities.size();
+        ComponentName[] set = new ComponentName[activityNum];
+        int defaultMatch = -1;
+        for(int i = 0; i < activityNum; i++){
+            ResolveInfo info = homeActivities.get(i);
+            set[i] = new ComponentName(info.activityInfo.packageName, info.activityInfo.name);
+            if(ORIGINAL_LAUNCHER_CLASSNAME.equals(info.activityInfo.name)
+                    && ORIGINAL_LAUNCHER_PACKAGENAME.equals(info.activityInfo.packageName)){
+                defaultMatch = info.match;
+            }
+        }
+
+        //if Launcher is not found, do not set anything
+        if(defaultMatch == -1){
+            return ;
+        }
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_MAIN);
+        filter.addCategory(Intent.CATEGORY_HOME);
+        filter.addCategory(Intent.CATEGORY_DEFAULT);
+
+        mPm.addPreferredActivity(filter, defaultMatch, set, defaultLauncher);
+    }
 }
